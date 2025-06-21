4529d43533ef502f1c81b351449256122f6e0fe1
SD-W1972 <pernalongaesnobe@gmail.com>
2025-06-21 12:07:41 -0300

    feat: adicionados metodos de status para classe PersonagemGame

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,48 @@
+
+public class PersonagemGame {
+	private int saudeAtual;
+	private String nome;
+	private String status;
+
+	public int getSaudeAtual() {
+		return saudeAtual;
+	}
+	
+	public void setSaudeAtual(int saudeAtual) {
+		this.saudeAtual = saudeAtual;
+		
+		if(this.saudeAtual > 0) {
+			this.status = "vivo";
+		}else {
+			this.status = "morto";
+		}
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void tomarDano(int quantidadeDeDano) {
+		if(this.saudeAtual - quantidadeDeDano < 0) {
+			setSaudeAtual(0);
+		}else {
+			setSaudeAtual(this.saudeAtual -= quantidadeDeDano);
+		}
+	}
+	
+	public void receberCura(int quantidadeDeCura) {
+		if(this.saudeAtual + quantidadeDeCura > 100) {
+			setSaudeAtual(100);
+		}else {
+			setSaudeAtual(this.saudeAtual += quantidadeDeCura);
+		}
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+}
