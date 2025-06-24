e711d6771f7c1e4c9b8c7a73408b538b26eb03a1
SD-W1972 <pernalongaesnobe@gmail.com>
2025-06-24 19:07:46 -0300

    feat: mudando metodos de preco liquido. Testando estrutura condicional para definicao do valor do livro Duna

--- Heranca_Exercicios/resumo_pedido/produtos/Produto.java
+++ Heranca_Exercicios/resumo_pedido/produtos/Produto.java
@@ -1,16 +1,29 @@
 package produtos;
 
 public abstract class Produto {
-	public String titulo;
-	int ano;
-	String pais;
-	double precoBruto;
-	
-	public abstract double obterPrecoLiquido();
-	public Produto(String titulo, int ano, String pais, double precoBruto) {
-		this.titulo = titulo;
-		this.ano = ano;
-		this.pais = pais;
-		this.precoBruto = precoBruto;
-	}
+    protected String titulo;
+    protected int ano;
+    protected String pais;
+    protected double precoBruto;
+    
+    public Produto(String titulo, int ano, String pais, double precoBruto) {
+        this.titulo = titulo;
+        this.ano = ano;
+        this.pais = pais;
+        this.precoBruto = precoBruto;
+    }
+    
+    public abstract double obterPrecoLiquido();
+    
+    // Getters
+    public String getTitulo() { return titulo; }
+    public int getAno() { return ano; }
+    public String getPais() { return pais; }
+    public double getPrecoBruto() { return precoBruto; }
+    
+    // Setters
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public void setAno(int ano) { this.ano = ano; }
+    public void setPais(String pais) { this.pais = pais; }
+    public void setPrecoBruto(double precoBruto) { this.precoBruto = precoBruto; }
 }
