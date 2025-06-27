93394ebe457d8d1e8640979bd8fc687e81a85e58
SD-W1972 <pernalongaesnobe@gmail.com>
2025-06-27 09:50:27 -0300

    feat: tasks sobre tratamento de exceção

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,15 @@
+
+public class Numero {
+
+	public static void dividir(int a, int b) {
+		int divisao = 0;
+		try {
+			 divisao = a/b;
+
+		}catch(Exception e) {
+			System.out.println("Nao eh possivel dividir por zero");
+		}finally {
+			System.out.println(a + " / " + b + " = " + divisao);
+		}
+	}
+}
