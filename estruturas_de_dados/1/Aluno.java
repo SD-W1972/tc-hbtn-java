ed7161b05cee384b7ac54865f3bcc3dad4469ecd
SD-W1972 <pernalongaesnobe@gmail.com>
2025-07-02 12:19:07 -0300

    feat: testando approach task 1 projeto estrutura de dados

---
+++ estruturas_de_dados/1/Aluno.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class Aluno{
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+
+    public Aluno(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public Aluno(){
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return idade == aluno.idade && Objects.equals(nome, aluno.nome);
+    }
+    
+}
