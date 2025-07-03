54aec4be03ab3bbe9c2a71b052200215524c801e
SD-W1972 <anthonybemol@gmail.com>
2025-07-03 11:24:25 +0000

    feat: revisao para projeto Classes_Objetos

--- Classes_Objetos/consistencia/PersonagemGame.java
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -1,40 +1,41 @@
-
 public class PersonagemGame {
-	private int saudeAtual;
-	private String nome;
-	private String status;
+    private int saudeAtual;
+    private String nome;
+    private String status;
 
-	public PersonagemGame(int saudeInicial, String nomeInicial) {
-		setSaudeAtual(saudeInicial);
-		this.nome = nomeInicial;
-	}
-	
-	public int getSaudeAtual() {
-		return saudeAtual;
-	}
-	
-	public void setSaudeAtual(int saudeAtual) {
-		this.saudeAtual = saudeAtual;
-		
-		if(this.saudeAtual > 0) {
-			this.status = "vivo";
-		}else {
-			this.status = "morto";
-		}
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		
-		  if(nome != null && !nome.isEmpty()) {
-	            this.nome = nome;
-	        }
-	}
+    public PersonagemGame(int saude, String nome){
+        setSaudeAtual(
+            saude
+        );
+        setNome(
+            nome
+        );
+    }
 
-	public void tomarDano(int quantidadeDeDano) {
+
+    public int getSaudeAtual(){
+        return this.saudeAtual;
+    }
+    public void setSaudeAtual(int saude){
+        this.saudeAtual = saude;
+        if(this.saudeAtual > 0){
+            this.status = "vivo";
+        }else{
+            this.status = "morto";
+        }
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+            if(nome != null && !nome.isEmpty()){   
+                this.nome = nome;
+            }
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
 		if(this.saudeAtual - quantidadeDeDano < 0) {
 			setSaudeAtual(0);
 		}else {
