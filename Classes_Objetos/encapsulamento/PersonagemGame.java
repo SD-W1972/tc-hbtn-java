b2249211e0551633c5231b3d22c756ef53068a98
SD-W1972 <anthonybemol@gmail.com>
2025-07-03 11:32:03 +0000

    feat: testando remocao da pasta encapsulamento para verificacao com checker

--- Classes_Objetos/encapsulamento/PersonagemGame.java
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -1,21 +1,20 @@
-
 public class PersonagemGame {
-	private int saudeAtual;
-	private String nome;
+    private int saudeAtual;
+    private String nome;
+    
+    public int getSaudeAtual(){
+        return this.saudeAtual;
+    }
+    public void setSaudeAtual(int saude){
+        this.saudeAtual = saude;
 
-	public int getSaudeAtual() {
-		return saudeAtual;
-	}
-	
-	public void setSaudeAtual(int saudeAtual) {
-		this.saudeAtual = saudeAtual;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 }
--- Classes_Objetos/personagem/PersonagemGame.java
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -1,22 +1,7 @@
+public class PersonagemGame{
+    int saudeAtual;
+    String nome;
 
-public class PersonagemGame {
-	private int saudeAtual;
-	private String nome;
 
-	public int getSaudeAtual() {
-		return saudeAtual;
-	}
-	
-	public void setSaudeAtual(int saudeAtual) {
-		this.saudeAtual = saudeAtual;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
+
 }
