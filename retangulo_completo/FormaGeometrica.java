b6561285ab73faa0a5d5ce7b71c06824fa680084
SD-W1972 <anthonybemol@gmail.com>
2025-07-03 12:26:31 +0000

    feat: task 4 projeto heranca

---
+++ retangulo_completo/FormaGeometrica.java
@@ -0,0 +1,7 @@
+import java.lang.UnsupportedOperationException;
+
+public class FormaGeometrica {
+	public double area() {
+		throw new UnsupportedOperationException("Funcao area nao implementada");
+	}
+}
