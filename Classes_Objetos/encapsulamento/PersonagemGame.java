91809ab0f4113bf3e15f13a99df6d5a9be259bb2
SD-W1972 <anthonybemol@gmail.com>
2025-07-03 11:41:36 +0000

    feat: revisao final projeto Classes_Objetos

---
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -0,0 +1,19 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    
+    public int getSaudeAtual(){
+        return this.saudeAtual;
+    }
+
+    public void setSaudeAtual(int saude){
+        this.saudeAtual = saude;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String n){
+        this.nome = n;
+    }
+}
