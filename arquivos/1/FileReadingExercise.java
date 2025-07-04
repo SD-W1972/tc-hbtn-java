16b36b498f5d16f79a6b023c3d896841c1215c92
SD-W1972 <anthonybemol@gmail.com>
2025-07-04 14:24:54 +0000

    feat: correcao organizao de pastas + task 1 e 2 projeto file handling

--- arquivos/1/FileReadingExercise.java
+++ arquivos/1/FileReadingExercise.java
@@ -1,35 +1,28 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 
-public class FileReadingExercise{
-    public static void main(String[] args)  throws IOException, FileNotFoundException{
-        String fileName = "funcionarios.csv"; // Nome do arquivo fixo para leitura
+public class FileReadingExercise {
+    public static void main(String[] args) throws IOException{
+        String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
 
-
-        // efetue a leitura do arquivo
-        BufferedReader bf;
         String linha;
-        String[] items;
-        try{
-            bf =  new BufferedReader(new FileReader(fileName));
-            while((linha = bf.readLine()) != null)
-            {   
-              items = linha.split(",");
-              System.out.println("Funcionário: " + items[0]);
-              System.out.println("Idade: " + items[1]);
-              System.out.println("Departamento: " + items[2]);
-              System.out.println("Salarial: " + items[3]);
-              System.out.println("------------------------");
-              
-              }
-            
-        }catch(IOException e){
-            throw new IOException("Ocorreu um erro ao ler o arquivo");
-        }
-
-        System.out.println("Leitura do arquivo concluída.");
+        // efetue a leitura do arquivo
+        System.out.println("Conteúdo do arquivo 'exemplo.txt':");
+        System.out.println();
+    try{
+        BufferedReader bf = new BufferedReader(new FileReader(fileName));
+        while((linha = bf.readLine()) != null){
+            System.out.println(linha);
+        }    
+    }catch(IOException e){
+        throw new IOException("Nao foi possivel ler o arquivo");
     }
+        System.out.println();
+    System.out.println("Leitura do arquivo concluída.");
+    }
+
+
+    
 }
