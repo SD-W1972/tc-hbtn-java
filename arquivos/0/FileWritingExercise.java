94224c84548afb310c785ace06e20b59f77f39ee
SD-W1972 <pernalongaesnobe@gmail.com>
2025-07-11 09:28:18 -0300

    feat: projeto File handling task 0

---
+++ arquivos/0/FileWritingExercise.java
@@ -0,0 +1,37 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileWritingExercise {
+    public static void main(String[] var0) {
+        Scanner var1 = new Scanner(System.in);
+        System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+        String var2 = var1.nextLine();
+
+        try (BufferedWriter var3 = new BufferedWriter(new FileWriter(var2))) {
+            System.out.println("Digita umas coisas maneiras");
+
+            while(true) {
+                System.out.print("> ");
+                String var4 = var1.nextLine();
+                if (var4.equalsIgnoreCase("sair")) {
+                    System.out.println("Arquivo '" + var2 + "' foi criado e seu conteúdo foi salvo");
+                    break;
+                }
+
+                var3.write(var4);
+                var3.newLine();
+            }
+        } catch (IOException var8) {
+            System.err.println("Erro ao escrever no arquivo: " + var8.getMessage());
+        }
+
+        var1.close();
+    }
+}
