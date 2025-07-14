1ca082d32c7fd49ace7d998aa51dad66a0444c9d
SD-W1972 <anthonybemol@gmail.com>
2025-07-14 11:34:40 +0000

    feat: adicionando tratamento de excecao

--- List/numeros/ManipularArrayNumeros.java
+++ List/numeros/ManipularArrayNumeros.java
@@ -12,7 +12,7 @@ public class ManipularArrayNumeros  {
 
     public static void adicionarNumero(List<Integer> lista, int n) throws Exception{
         if(buscarPosicaoNumero(lista, n) != -1){
-           System.err.println("Numero jah contido na lista");
+          throw new Exception("Numero jah contido na lista");
         }else{
             lista.add(n);
         }
@@ -21,7 +21,7 @@ public class ManipularArrayNumeros  {
 
     public static void removerNumero(List<Integer> lista, int n) throws Exception{
         if(buscarPosicaoNumero(lista, n) == -1){
-            System.err.println("Numero nao encontrado na lista");
+            throw new Exception("Numero nao encontrado na lista");
         }else{
             int p = buscarPosicaoNumero(lista, n);
             lista.remove(p);
