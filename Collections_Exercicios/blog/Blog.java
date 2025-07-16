b955a21a3479e3309e167e665bc8aec06e593e5a
SD-W1972 <anthonybemol@gmail.com>
2025-07-16 13:27:15 +0000

    feat: task 0 projeto Collections Exercicios

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Blog{
+    List<Post> lista = new ArrayList<>();
+
+    public void adicionarPostagem(Post post){
+        lista.add(post);
+    }
+
+    public Set<String> obterTodosAutores(){
+        Collections.sort(lista);
+        Set<String> autores = new HashSet<>();
+        for(Post p : lista){
+            autores.add(p.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria(){
+       Map<String, Integer> categoriaQuantidade = new TreeMap<>();
+        for(Post p : lista){
+
+            categoriaQuantidade.put(p.getCategoria(), categoriaQuantidade.getOrDefault(p.getCategoria(), 0) + 1 );
+       }
+       return categoriaQuantidade;
+    }
+}
---
+++ Collections_Exercicios/blog/Post.java
@@ -0,0 +1,30 @@
+import java.lang.Comparable;
+
+public class Post implements Comparable<Post>{
+    private String autor;
+    private String titulo;
+    private String corpo;
+    private String categoria;
+
+    public String getAutor(){return autor;}
+    public String getTitulo(){return titulo;}
+    public String getCorpo(){return corpo;}
+    public String getCategoria(){return categoria;}
+
+    public void setAutor(String a){this.autor = a;}
+    public void setTitulo(String t){this.titulo = t;}
+    public void setCorpo(String c){this.corpo = c;}
+    public void setCategoria(String ca){this.categoria = ca;}
+
+    public Post(String a, String t, String c, String ca){
+        this.autor = a;
+        this.titulo = t;
+        this.corpo = c;
+        this.categoria = ca;
+    }
+
+    @Override
+    public int compareTo(Post post){
+        return this.autor.compareTo(post.getAutor());
+    }
+}
