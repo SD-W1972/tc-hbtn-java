dd7f9aa8f3a36a4dd4e11b733edf09346aaec3c2
SD-W1972 <pernalongaesnobe@gmail.com>
2025-07-21 13:19:28 -0300

    feat: task 0 projeto Streams

---
+++ Streams_Collectors_Optional/livros/Consulta.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Consulta{
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+        List<Produto> produtosFiltrados = pedido.getProdutos();
+
+       return produtosFiltrados.stream()
+                .filter(Produto -> Produto.getCategoria().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+
+    }
+}
---
+++ Streams_Collectors_Optional/livros/Produto.java
@@ -0,0 +1,50 @@
+public class Produto{
+    private String nome;
+    private CategoriaProduto categoria;
+    private double preco;
+    private int codigo;
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return String.format("[%d] %s %s R$ %.2f", codigo, nome, categoria, preco);
+    }
+
+    public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaProduto categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+}
