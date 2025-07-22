2a220506246fb8f725538ccec58202aef971cdb6
SD-W1972 <pernalongaesnobe@gmail.com>
2025-07-22 13:12:42 -0300

    feat: task 1 projeto java collectors

---
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.List;
+
+public class ConsultaPessoas{
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas){
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo));
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas){
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+    }
+}
