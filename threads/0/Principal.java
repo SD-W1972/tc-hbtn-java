08deb300fafd60036505b030fa1a93ff3c5c6a5c
SD-W1972 <pernalongaesnobe@gmail.com>
2025-07-24 12:16:50 -0300

    feat: task 0 projeto multi threading

---
+++ threads/0/Principal.java
@@ -0,0 +1,16 @@
+import java.lang.Thread;
+
+public class Principal{
+    public static void main(String[] args){
+        Contador contador = new Contador();
+        ThreadContador threadContador = new ThreadContador(contador);
+
+        Thread t0 = new Thread(threadContador);
+        Thread t1 = new Thread(threadContador);
+
+        t0.run();
+        t1.run();
+
+        System.out.println("Valor final do contador: " + contador.getContagem());
+    }
+}
