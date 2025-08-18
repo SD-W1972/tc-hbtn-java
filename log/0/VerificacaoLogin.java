93339288116dafd777a954015c4d0026032b6f2a
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-18 09:36:13 -0300

    feat: logs implementados

--- log/0/VerificacaoLogin.java
+++ log/0/VerificacaoLogin.java
@@ -1,2 +1,34 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class VerificacaoLogin {
+
+
+    // Inicialização do logger
+    private static final Logger logger = LoggerFactory.getLogger(VerificacaoLogin.class);
+
+
+    public static void main(String[] args) {
+        // Simulação de entradas do usuário
+        String usuarioCorreto = "admin";
+        String senhaCorreta = "12345";
+
+
+        // Tentativas de login
+        realizarLogin("admin", "12345", usuarioCorreto, senhaCorreta); // Login correto
+        realizarLogin("admin", "senhaErrada", usuarioCorreto, senhaCorreta); // Senha incorreta
+        realizarLogin("usuarioDesconhecido", "12345", usuarioCorreto, senhaCorreta); // Usuário desconhecido
+    }
+
+
+    public static void realizarLogin(String usuario, String senha, String usuarioCorreto, String senhaCorreta) {
+        // implemente aqui o log
+        logger.info("Aplicação iniciada");
+        logger.info("Tentativa de login com o usuário: " + usuarioCorreto);
+        logger.info("Login bem-sucedido para o usuário: " + usuarioCorreto);
+        logger.info("Tentativa de login com o usuário: " + usuarioCorreto);
+        logger.error("Senha incorreta para o usuário: " + usuarioCorreto);
+        logger.warn("Usário usuarioDesconhecido não encontrado!");
+    }
 }
