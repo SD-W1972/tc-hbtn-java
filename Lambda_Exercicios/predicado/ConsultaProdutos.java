5c3aa958a22e6ce7675c9241bd949985b11da26c
SD-W1972 <anthonybemol@gmail.com>
2026-03-09 07:46:59 -0300

    feat: refactoring some old stuff

--- Lambda_Exercicios/predicado/ConsultaProdutos.java
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -1,18 +1,15 @@
 import java.util.function.Predicate;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class ConsultaProdutos{
     public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> testar){
         List<Produto> produtosFiltrados = new ArrayList<>();
 
-        for(Produto p : produtos){
-
-            if(testar.test(p)){
-                produtosFiltrados.add(p);
-            }
-        }
-
-        return produtosFiltrados;
+        produtosFiltrados.addAll(produtos);
+        return produtosFiltrados.stream()
+        .filter(testar)
+        .collect(Collectors.toList());
     }
 }
