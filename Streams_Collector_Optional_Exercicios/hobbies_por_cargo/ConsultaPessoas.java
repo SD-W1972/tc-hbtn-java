eb576deb4f578bec79a7e6f4b28a34a3d4390a58
SD-W1972 <anthonybemol@gmail.com>
2026-03-11 08:51:37 -0300

    feat: using flatMapping with collectors and streams

--- Streams_Collector_Optional_Exercicios/hobbies_por_cargo/ConsultaPessoas.java
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/ConsultaPessoas.java
@@ -43,14 +43,14 @@ public class ConsultaPessoas{
                 ));
     }
 
-    public static Map<String, TreeSet<String>> obterHobbiesPorCargo(List<Pessoa> pessoas){
-        return pessoas.stream()
+        public static Map<String, TreeSet<String>> obterHobbiesPorCargo(List<Pessoa> pessoas){
+                return pessoas.stream()
                 .collect(Collectors.groupingBy(
                         Pessoa::getCargo,
                         Collectors.flatMapping(
-                                pessoa -> pessoa.getHobbies().stream()
-                                , Collectors.toCollection(TreeSet::new)
+                                (pessoa) -> pessoa.getHobbies().stream(),
+                                Collectors.toCollection(TreeSet::new)
                         )
                 ));
-    }
+        }
 }
