bd35f2e0443aa991ff37e510eba2d127f8e1ac69
SD-W1972 <anthonybemol@gmail.com>
2026-03-11 08:29:22 -0300

    feat: remembering some stuff about streams collectos and grouping by

--- Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -7,21 +7,21 @@ import java.util.Map;
 
 public class ConsultaPessoas{
     public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas){
-        TreeMap<String, TreeSet<Pessoa>> pessoTree = pessoas.stream()
+        TreeMap<String, TreeSet<Pessoa>> pessoaTree = pessoas.stream()
                 .collect(Collectors.groupingBy(
                         Pessoa::getCargo,
                         () -> new TreeMap<>(Collections.reverseOrder()),
                         Collectors.toCollection(TreeSet::new)
                 ));
-        return pessoTree;
+        return pessoaTree;
     }
 
     public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas){
         return pessoas.stream()
-                .collect(Collectors.groupingBy(
-                        Pessoa::getCargo,
-                        Collectors.counting()
-                ));
+        .collect(Collectors.groupingBy(
+                Pessoa::getCargo,
+                Collectors.counting()
+        ));
     }
 
 
