3c8b6846b55dffe94c4654138f0e3f77025683ec
SD-W1972 <anthonybemol@gmail.com>
2026-03-13 10:43:12 -0300

    feat: revisioning some stuff about multithreads

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -2,24 +2,21 @@ import java.lang.Thread;
 import java.lang.InterruptedException;
 
 public class Principal{
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         Contador contador = new Contador();
-        ThreadContador threadContador = new ThreadContador(contador);
+        ThreadContador t1 = new ThreadContador(contador);
+        ThreadContador t2 = new ThreadContador(contador);
 
-        Thread t0 = new Thread(threadContador);
-        Thread t1 = new Thread(threadContador);
+        t1.start();
+        synchronized(t1){
+            t1.wait();
+        }
 
-       t0.start();
-       t1.start();
-
-       try{
-            t0.join();
-            t1.join();
-
-       }catch(InterruptedException e){
-           e.printStackTrace();
-       }
-
-        System.out.println("Valor final do contador: " + contador.getContagem());
-    }
+        t2.start();
+        synchronized(t2){
+            t2.wait();
+            t2.notify();
+        }
+        System.out.println(contador.getContagem());    
+    }    
 }
